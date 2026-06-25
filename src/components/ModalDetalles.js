e0bdaea8af5f49c15bb25b79e4fc47d4f3068b99
Tammy Shabetay <tammyshabetay@Air-de-Tammy.mshome.net>
2026-06-25 12:38:01 -0500

    Fix input height - use fixed height so modal doesn't resize with long text

--- src/components/ModalDetalles.js
+++ src/components/ModalDetalles.js
@@ -95,7 +95,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   input: {
-    minHeight: 200,
+    height: 200,
     borderWidth: 1,
     borderColor: colors['outline-variant'],
     borderRadius: 12,
